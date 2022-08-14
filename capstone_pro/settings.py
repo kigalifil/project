0864803ce46f940b627f89bd7fd411762da8ef30
ndayise <ndayisengaelyse@gmail.com>
2022-08-14 01:57:01 -0700

    first commit

--- capstone_pro/settings.py
+++ capstone_pro/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1)zhf@4(89$fdp_@8nhgk95cvwzcbwwzwx!1dj5jtru0_1cms#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.0.0.3','.herokuapp.com']
 
 
 # Application definition
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'capstone_pro.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':'db.sqlite3',
     }
 }
 
