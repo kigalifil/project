2dfcfbeef8ee49ca8256189db7c5223827fd2ffb
ndayise <ndayisengaelyse@gmail.com>
2022-08-14 02:53:21 -0700

    first commits

--- capstone_app/urls.py
+++ capstone_app/urls.py
@@ -9,7 +9,7 @@ from.views import (
 )
 from . import views
 urlpatterns = [
-    path('index', PostListView.as_view(), name='index'),
+    path('', PostListView.as_view(), name='index'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
